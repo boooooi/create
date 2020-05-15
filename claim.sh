39be2a19153ba0f2af8e59d8fb5e07e3206cb53c
boooooi <56160031+boooooi@users.noreply.github.com>
2020-05-15 14:53:51 +0300

    Update claim.sh

--- claim.sh
+++ claim.sh
@@ -2,13 +2,10 @@
 
 sleepCounter="3"
 loopCounter="1000" # Select amount of wallets (iterations) script will create
-node="https://api.s0.os.hmny.io/"
 
 echo "########################################## Starting $loopCounter script iterations for creating wallets"
 
 i="0"
-
-./hmy balances --node="https://api.s0.os.hmny.io" $initialAddress | tee -a balance_before_after #Checking balance before and writing to file
 
 while [ $i -lt $loopCounter ] # Number of total loops
 do
